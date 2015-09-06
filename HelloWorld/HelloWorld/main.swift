f2e3082d040e74bb16177d83fd350298b938b4f2
Darrell Little <darrell.little@gmail.com>
2015-09-06 13:22:23 -0400

    First HelloWorld with Xcode

---
+++ HelloWorld/HelloWorld/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by Darrell Little on 9/6/15.
+//  Copyright (c) 2015 Darrell Little. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+print("Helloooooo ... ")
+println("Ok, that's it, done!")
